5a10e24e7ac1eaab7105406153d3edce9e016bae
Davide Di Criscito <davide.dicriscito@gmail.com>
2017-12-10 14:55:09 +0100

    update to commonjs/AMD

--- README.md
+++ README.md
@@ -1,18 +1,5 @@
 # JSONslider
-JSONslider is a simple jQuery slider for pictures stored dynamically in a JSON file you can edit without messing up your HTML..
-
-<hr />
-
-<a href="https://flattr.com/submit/auto?user_id=dcdeiv&url=http%3A%2F%2Fwww.github.com%2Fdcdeiv%2Fjsonslider" target="_blank"><img src="http://button.flattr.com/flattr-badge-large.png" alt="Flattr this" title="Flattr this" border="0"></a>
-
-<hr />
-
-**This is a beta version, it has not been fully tested**
-
-## DOWNLOAD
-
-* Git: `$ git https://github.com/dcdeiv/jsonslider.git`
-* Bower: `$ bower install jsonslider`
+JSONslider is a simple jQuery slider for pictures stored dynamically in a JSON file you can edit without messing up your HTML.
 
 ## SET UP
 
@@ -26,47 +13,14 @@ The JSON file must have at least these names and values:
       }
     ] }
 
-Repeat for other pictures.
-In the [store.json](store.json) file you will find along with the plug-in, is just an example of a `*.json` file. Other names in that file are just for storing more information.
-
 ### 2 - Set HTML tag
-Give a unique ID or a class to an empty `<div>` in your `HTML`:
+Give a unique ID or a class to an empty `<div>` in your `HTML`, embed the json file in the data attr and set the desired aspect-ratio:
 
-  <div class="your-class"></div>
-  
+  <div id="slider-1" data-json="resources/store.json" data-aspect-ratio="16:9"></div>
+
 ### 3 - Run the function
 This is an example of how the function must be called with all available options.
 
     $( document ).ready(function() {
-      	$( '.your-class' ).jsonSlider({
-      		json: 'path-to-your-json-file.json',
-      		Class: 'slider-active', //default class
-      		aspectRatio: '16:9', //aspect ratio of the slider. Chose it according to pictures aspect ratio.
-      		css: { //don't touch these parametres
-      			parent: {
-      				position: 'relative'
-				},
-				wrap: {
-					position: 'relative',
-					width: '100%',
-					height: '100%',
-					margin: '0 auto',
-					padding: 0,
-					backgroundColor: 'inherit',
-					overflow: 'hidden'
-				},
-				figure: {
-					position: 'absolute'
-				},
-				img: {
-					width: 'auto',
-					maxWidth: '100%',
-					lineHeight: 0,
-					margin: '0 auto'
-				}
-			}
-      	});
+      	$( '#slider' ).jsonSlider();
     });
-
-For compatibility issues I added the option `Class` that lets you set a custom class to call the *active element*.
-By default the class is `'slider-active`.
---
+++ dist/jquery.jsonslider.js
@@ -0,0 +1,145 @@
+/*!
+ * jsonslider - A simple jQuery slider for pictures stored dinamically in a JSON file you can edit without messing up your HTML
+ * @version v1.0.0
+ * @link https://github.com/dcdeiv/jsonslider#readme
+ * @copyright (c) 2015 - 2017 Davide Di Criscito <davide.dicriscito@gmail.com> (https://github.com/dvdeiv)
+ * @license MIT
+ */
+// Uses CommonJS, AMD or browser globals to create a jQuery plugin.
+( function( factory ) {
+	if ( typeof define === "function" && define.amd ) {
+
+		// AMD. Register as an anonymous module.
+		define( [ "jquery" ], factory );
+	} else if ( typeof module === "object" && module.exports ) {
+
+		// Node/CommonJS
+		module.exports = function( root, jQuery ) {
+			if ( jQuery === undefined ) {
+				if ( typeof window !== "undefined" ) {
+					jQuery = require( "jquery" );
+				}			else {
+					jQuery = require( "jquery" )( root );
+				}
+			}
+			factory( jQuery );
+			return jQuery;
+		};
+	} else {
+
+		// Browser globals
+		factory( jQuery );
+	}
+}( function( $ ) {
+
+	// Set the plugin name
+	var pluginName = "jsonSlider";
+
+	$.fn[ pluginName ] = function( options ) {
+
+		var def = $.extend( true, {
+			css: {
+				parent: {
+					position: "relative"
+				},
+				wrap: {
+					position: "relative",
+					width: "100%",
+					height: "100%",
+					margin: "0 auto",
+					padding: 0,
+					backgroundColor: "inherit",
+					overflow: "hidden"
+				},
+				figure: {
+					position: "absolute"
+				},
+				img: {
+					width: "auto",
+					maxWidth: "100%",
+					lineHeight: 0,
+					margin: "0 auto"
+				}
+			}
+		}, options );
+
+		return this.each( function() {
+			var $parentW, $parentH, $wrap,
+				cfg = $.extend( true, def, options ),
+				$parent = $( this ),
+				json = $parent.data( "json" ),
+				aspect = $parent.data( "aspect-ratio" ),
+				css = cfg.css,
+				AR = aspect.split( ":" ),
+				ARx = parseInt( AR[ 0 ] ),
+				ARy = parseInt( AR[ 1 ] ),
+				arI = ARx / ARy;
+
+			$parentW = parseInt( $parent.css( "width" ) );
+			$parentH = parseInt( $parent.css( "height" ) );
+
+			$parent.append( "<div>" );
+			$wrap = $parent.children();
+
+			$( window ).resize( function() {
+				var parentStyle = {
+						height: $parentW / arI
+					},
+					newStyle = $.extend( {}, css.parent, parentStyle );
+
+				if ( $parentH === 0 ) {
+					$parent.css( newStyle );
+				}
+
+				$wrap.css( css.wrap );
+			} );
+
+			$.getJSON( json, function( data ) {
+				var $figs, $first, i,
+					arr = $.map( data, function( el ) {
+						return el;
+					} );
+
+				for ( i = 0; i < arr.length; i++ ) {
+					var $img = "<figure><img src='" +
+						arr[ i ].url + "' alt='" +
+						arr[ i ].alt + "'/></figure>";
+
+					$wrap.append( $( $img ) );
+				}
+
+				$figs = $wrap.children();
+				$figs.css( css.figure );
+
+				$first = $figs.first();
+				$first.attr( "data-status", "active" );
+
+				$figs.not( $first ).hide();
+
+				function slider() {
+					var $next,
+						$active = $wrap.find( "[data-status='active']" );
+
+					if ( $active.next().length === 0 ) {
+						$next = $first;
+					} else {
+						$next = $active.next();
+					}
+
+					$active.fadeOut( 1000, function() {
+						$( this ).attr( "data-status", "inactive" );
+					} );
+					$next.fadeIn( 1000, function() {
+						$( this ).attr( "data-status", "active" );
+					} );
+				}
+
+				setTimeout( setInterval( function() {
+					slider();
+				}, 4000 ), 5000 );
+
+				$( window ).resize();
+			} );
+		} );
+	};
+} ) );
